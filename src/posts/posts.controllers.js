de2a62a4697e0e50f713d3f95aee8f137bc412a9
Juan Camilo Sierra Rojas <camilo.sierra@co.onelink.bpo>
2022-10-29 12:28:39 -0500

    first commit

--- src/posts/posts.controllers.js
+++ src/posts/posts.controllers.js
@@ -1,8 +1,27 @@
 const Posts = require("../models/posts.models");
+const Users = require("../models/users.models");
+const Categories = require("../models/categories.models");
 const uuid = require("uuid");
 
 const getAllPost = async () => {
-  const data = await Posts.findAll();
+  const data = await Posts.findAll({
+    include: [
+      {
+        model: Users,
+      },
+      {
+        model: Categories,
+        // Exclude some columns on relations
+        // attributes: {
+        //   exclude: ["id"],
+        // },
+      },
+    ],
+    // Exclude some columns on data
+    // attributes: {
+    //   exclude: ["createdAt", "updatedAt", "categoryId"],
+    // },
+  });
   return data;
 };
 
